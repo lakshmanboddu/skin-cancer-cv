226ac8e45dc2c0575beee8b542ab6ec5d9d03c97
Lakshman Boddu <lakshmanboddu@gmail.com>
2020-02-29 18:06:51 -0500

    update files again

--- src/main/java/org/finalProj/App.java
+++ src/main/java/org/finalProj/App.java
@@ -6,11 +6,11 @@ public class App {
 
     public static void main(String[] args) {
 
-//        HairRemoval.removeHair();
+        HairRemoval.removeHair();
 //        AsymmetryDetection obj = new AsymmetryDetection();
 //        System.out.println(obj.symmetry);
 //        Border border = new Border();
-        ColorProperty colorProperty = new ColorProperty();
+//        ColorProperty colorProperty = new ColorProperty();
 //        Diameter d = new Diameter();
 
     }
--- src/main/java/org/finalProj/HairRemoval.java
+++ src/main/java/org/finalProj/HairRemoval.java
@@ -3,9 +3,15 @@ package org.finalProj;
 
 import org.bytedeco.opencv.opencv_core.Mat;
 
-import static org.bytedeco.opencv.global.opencv_core.*;
-import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
-import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_core.CV_32F;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.MORPH_BLACKHAT;
+import static org.bytedeco.opencv.global.opencv_imgproc.THRESH_BINARY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.morphologyEx;
+import static org.bytedeco.opencv.global.opencv_imgproc.threshold;
 import static org.bytedeco.opencv.global.opencv_photo.INPAINT_TELEA;
 import static org.bytedeco.opencv.global.opencv_photo.inpaint;
 
@@ -27,6 +33,7 @@ public class HairRemoval {
         Mat blackHat = new Mat();
         morphologyEx(grayScaleImg,blackHat, MORPH_BLACKHAT,kernel);
         imwrite("src/resources/blackHat-output.jpg", blackHat);
+        Display.display(blackHat, "Blackhat image");
 
         //Intensifying the hair countours in preparation for the inpainting algorithm
         Mat thresholdedImage = new Mat();
